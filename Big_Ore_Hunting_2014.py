863849066adb7c559d29d7d86fbbd93b0cf26327
Brandon Sturgeon <sturgeonb4@gmail.com>
2014-05-01 11:21:36 -0500

    Introduced block-breaking

--- Big_Ore_Hunting_2014.py
+++ Big_Ore_Hunting_2014.py
@@ -3,16 +3,59 @@
 # Brandon Sturgeon    #
 #######################
 
+import random
 import pygame
 pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+class WorldContainer(pygame.Surface):
+    def __init__(self, width, height, pos=(0, 0)):
+        pygame.Surface.__init__(self, (width, height))
+        self.width = width
+        self.height = height
+        self.pos = pos
+        self.rect = pygame.Rect(self.pos, (self.width, self.height))
+
+        self.objects = {}
+
+
+class GroundChunk(pygame.sprite.Sprite):
+    def __init__(self, pos, color=(0, 0, 0)):
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = pos
+        self.color = color
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(self.color)
+
+        self.rect = pygame.Rect(self.pos, self.image.get_size())
+
+    def update(self, key, player):
+        if key == pygame.K_w:
+            self.rect.y += 50
+        if key == pygame.K_a:
+            self.rect.x += 50
+        if key == pygame.K_s:
+            self.rect.y -= 50
+        if key == pygame.K_d:
+            self.rect.x -= 50
+
+        if self.rect.colliderect(player.rect):
+            print "COLLIDING"
+            self.kill()
 
 
 class Digger():
     def __init__(self):
-        self.image = pygame.Surface((100, 100))
-        self.image.fill((255, 0, 0))
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 0, 0))
         self.global_x = 0
         self.global_y = 0
+        self.rect = pygame.Rect((640-(self.image.get_width()/2),
+                                 400-(self.image.get_height()/2)), self.image.get_size())
 
         self.inventory = []
         
@@ -43,50 +86,82 @@ class Monster():
 class Game():
     def __init__(self):
         self.game_window = pygame.display.set_mode((1280, 800))
-        self.above_world = pygame.Surface((6400, 400))
-        self.above_world_rect = pygame.Rect((-3200, 0), self.above_world.get_size())
-        self.below_world = pygame.Surface((6400, 2400))
-        self.below_world_rect = pygame.Rect((-3200, 400), self.below_world.get_size())
-        self.world_rects = (self.above_world_rect, self.below_world_rect)
-        
+        self.above_world = WorldContainer(width=6400, height=400, pos=(-3200, 0))
+        self.below_world = WorldContainer(width=6400, height=2400, pos=(-3200, 400))
+        self.world_rects = (self.above_world.rect, self.below_world.rect)
+        self.ground_chunks = pygame.sprite.Group()
+
         self.bg_color = (135, 206, 250)
         self.player = Digger()
         self.font = pygame.font.Font(None, 30)
-        
+        self.playing = True
+
+        self.gen_ground_chunks()
         self.game_loop()
 
     def game_loop(self):
-        while True:
+        pygame.key.set_repeat(10, 10)
+        while self.playing:
             events = pygame.event.get()
             for event in events:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_s:
-                        self.player.global_y += 1
-                        for world in self.world_rects:
-                            world.y -= 5
-                    if event.key == pygame.K_w:
-                        self.player.global_y -= 1
-                        for world in self.world_rects:
-                            world.y += 5
-                    if event.key == pygame.K_a:
-                        self.player.global_x -= 1
-                        for world in self.world_rects:
-                            world.x += 5
-                    if event.key == pygame.K_d:
-                        self.player.global_x += 1
-                        for world in self.world_rects:
-                            world.x -= 5
+                if event.type == pygame.QUIT:
+                    self.playing = False
+                    break
 
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_s:
+                        if self.player.global_y*5 < 2350:
+                            self.player.global_y += 1
+                            self.ground_chunks.update(event.key, self.player)
+                            for world in self.world_rects:
+                                world.y -= 50
+                    elif event.key == pygame.K_w:
+                        if self.player.global_y*5 > -50:
+                            self.player.global_y -= 1
+                            self.ground_chunks.update(event.key, self.player)
+                            for world in self.world_rects:
+                                world.y += 50
+                    elif event.key == pygame.K_a:
+                        if self.player.global_x*5 > -3200:
+                            self.player.global_x -= 1
+                            self.ground_chunks.update(event.key, self.player)
+                            for world in self.world_rects:
+                                world.x += 50
+                    elif event.key == pygame.K_d:
+                        if self.player.global_x*5 < 3200:
+                            self.player.global_x += 1
+                            self.ground_chunks.update(event.key, self.player)
+                            for world in self.world_rects:
+                                world.x -= 50
+
+            self.game_window.fill(WHITE)
             self.above_world.fill(self.bg_color)
-            self.below_world.fill((0, 0, 0))
+            self.below_world.fill(BLACK)
             
-            self.game_window.blit(self.above_world, (self.above_world_rect.x, self.above_world_rect.y))
-            self.game_window.blit(self.below_world, (self.below_world_rect.x, self.below_world_rect.y))
+            self.game_window.blit(self.above_world, (self.above_world.rect.x, self.above_world.rect.y))
+            self.game_window.blit(self.below_world, (self.below_world.rect.x, self.below_world.rect.y))
+            self.ground_chunks.draw(self.game_window)
             self.game_window.blit(self.player.image, (640-(self.player.image.get_width()/2),
                                                       400-(self.player.image.get_height()/2)))
             self.game_window.blit(self.font.render("("+str(self.player.global_x)+"," +
                                                    str(self.player.global_y)+")", 1, (0, 255, 0)), (400, 10))
             pygame.display.flip()
+
+        pygame.quit()
+
+    def gen_ground_chunks(self):
+
+        print "Generating Chunks"
+        y_counter = 0
+        for y in range(int(2400/50)):
+            x_counter = -3200
+            y_counter += 50
+            for x in range(int(6400/50)):
+                rand_color = random.choice((RED, GREEN, BLUE))
+                self.ground_chunks.add(GroundChunk(color=rand_color, pos=(x_counter+15, y_counter+25)))
+                x_counter += 50
+
 
 
 if __name__ == "__main__":
